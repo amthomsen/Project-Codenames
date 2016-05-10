f4e80ec10fd1ec1f7a85c95f81f8ac19a4497e83
Andreas Møller Thomsen <amthomsen92@gmail.com>
2016-05-10 10:07:56 +0200

    Load words into array, generate table of cards

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+    Image _image;
+
+	void Awake() {
+        _image = GetComponent<Image>();
+	}
+
+    void Start()
+    {
+        _image.color = GameManager.instance.startColor;
+    }
+    
+    public void ChangeColor()
+    {
+        if (_image.color == GameManager.instance.startColor)
+        {
+            _image.color = GameManager.instance.red;
+        }
+        else if (_image.color == GameManager.instance.red)
+        {
+            _image.color = GameManager.instance.blue;
+        }
+        else if (_image.color == GameManager.instance.blue)
+        {
+            _image.color = GameManager.instance.grey;
+        }
+        else if (_image.color == GameManager.instance.grey)
+        {
+            _image.color = GameManager.instance.startColor;
+        }
+    }
+}
+
---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    public Color startColor, red, blue, grey;
+
+	// Use this for initialization
+	void Awake() {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ShiftColor()
+    {
+
+    }
+}
